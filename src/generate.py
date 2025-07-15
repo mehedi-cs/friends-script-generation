3ac252f77a34637e3bd3332f616bba8181592885
Md. Mehedi Hasan <mehedicsgo@gmail.com>
2025-07-15 13:09:12 -0400

    Update generate.py

--- src/generate.py
+++ src/generate.py
@@ -1,7 +1,3 @@
-import numpy as np
-import torch
-import torch.nn.functional as F
-
 def generate(rnn, prime_id, int_to_vocab, token_dict, pad_value, predict_len, sequence_length, device):
     rnn.eval()
     current_seq = np.full((1, sequence_length), pad_value)
